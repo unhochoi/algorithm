66be1f6821cbe0b7224b0878339898a01ad68cf7
unhochoi <yms04089@kookmin.ac.kr>
2022-01-28 13:18:25 +0900

    1149.cpp

--- dynamic/1149.cpp
+++ dynamic/1149.cpp
@@ -9,33 +9,14 @@ int cost[MAX][4];
 int accum_cost[MAX][4];
 
 // 2가지 값들 중, 최소값을 반환하는 함수
-int min_2(int a, int b){
-    if (a<=b){
-        return a;
-    }
-    else{
-        return b;
-    }
+int min(int a, int b){
+    return a < b ? a : b;
 }
 
-// 3가지 값들 중, 최소값을 반환하는 함수
-int min_3(int a, int b, int c){
-    if (a<b){
-        if (a<c){
-            return a;
-        }
-        else{
-            return c;
-        }
-    }
-    else{
-        if (b<c){
-            return b;
-        }
-        else{
-            return c;
-        }
-    }
+// 3가지 값들 중, 최소값을 반환하는 함수 (위 함수를 오버로딩)
+int min(int a, int b, int c){
+    int temp = min(a, b);
+    return temp < c ? temp : c;
 }
 
 // 1번째 집부터 N번째 집까지 칠하는 비용의 최소값을 출력하는 함수
@@ -54,26 +35,26 @@ int dp(int n){
             // i-1번째 집의 초록색, 파란색의 누적 비용 중, 최소 누적 비용을 구한 뒤
             // [i번째 빨간집의 비용 + i-1번째 집까지의 최소 누적 비용]으로 누적 비용을 갱신
             if (j==1){
-                 accum_cost[i][1] = cost[i][1] + min_2(accum_cost[i-1][2], accum_cost[i-1][3]);
+                 accum_cost[i][1] = cost[i][1] + min(accum_cost[i-1][2], accum_cost[i-1][3]);
             }
             // i번째 집을 초록색으로 칠한다면, 이전 집으로는 빨간색, 파란색이 가능하기 때문에, 
             // i-1번째 집의 빨간색, 파란색의 누적 비용 중, 최소 누적 비용을 구한 뒤
             // [i번째 초록집의 비용 + i-1번째 집까지의 최소 누적 비용]으로 누적 비용을 갱신
             else if (j==2){
-                accum_cost[i][2] = cost[i][2] + min_2(accum_cost[i-1][1], accum_cost[i-1][3]);
+                accum_cost[i][2] = cost[i][2] + min(accum_cost[i-1][1], accum_cost[i-1][3]);
             }
             // i번째 집을 파란색으로 칠한다면, 이전 집으로는 빨간색, 초록색이 가능하기 때문에, 
             // i-1번째 집의 빨간색, 초록색의 누적 비용 중, 최소 누적 비용을 구한 뒤
             // [i번째 파란색의 비용 + i-1번째 집까지의 최소 누적 비용]으로 누적 비용을 갱신
             else{
-                accum_cost[i][3] = cost[i][3] + min_2(accum_cost[i-1][1], accum_cost[i-1][2]);
+                accum_cost[i][3] = cost[i][3] + min(accum_cost[i-1][1], accum_cost[i-1][2]);
             }
         }
     }
 
     // 1번째 집부터 N번째 집까지 칠하는 비용 == N 번째 집의 누적 비용이므로,
     // N 번째 집의 누적 비용 중, 최소값을 반환
-    return min_3(accum_cost[n][1], accum_cost[n][2], accum_cost[n][3]);
+    return min(accum_cost[n][1], accum_cost[n][2], accum_cost[n][3]);
 }
 
 int main(){
