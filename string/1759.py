92c8d932ad47b248ae51ea53f263e4c380c50eaa
unhochoi <yms04089@kookmin.ac.kr>
2022-10-03 22:56:13 +0900

    암호 만들기

--- string/1759.py
+++ string/1759.py
@@ -12,6 +12,7 @@ a = []
 # 자음 리스트
 b = []
 
+# 문자들을 입력 받아, 모음과 자음 리스트에 추가
 chars = sys.stdin.readline().split()
 for char in chars:
     if (char in temp):
@@ -24,14 +25,20 @@ result = []
 
 # 모음이 i개일때, 자음은 l-i개 뽑기
 for i in range(1, l-1):
+    
     # 모음 조합
     a_combi = list(combinations(a, i))
     # 자음 조합
     b_combi = list(combinations(b, l-i))
+    
+    # 모음 조합과 자음 조합을 결합한 뒤, 정렬하고, 결과 리스트에 추가
     for j in a_combi:
         for k in b_combi:
             result.append(''.join(sorted(j + k)))
 
+# 결과 리스트 정렬
 result = sorted(result)
+
+# 결과 리스트 요소 출력
 for word in result:
     print(word)
